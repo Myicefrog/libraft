4a4f311a71bff6ee97080c1224f1072850269f53
·¹â <luguang@csc.com.cn>
2019-09-18 15:12:49 +0800

    更新代码，适配编译环境

--- TestInit/make.sh
+++ TestInit/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o TestDuelingCandidates TestDuelingCandidates.cc ../test/raft_test_util.cc -I ../include/ -I ../src/ -I ../test/ -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
+g++ -std=c++11 -o TestDuelingCandidates TestDuelingCandidates.cc ../test/raft_test_util.cc -I ../include/ -I ../src/ -I ../test -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
