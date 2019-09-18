da9f072562eda2dc80271f6e6adb6e4adf4b741e
·¹â <luguang@csc.com.cn>
2019-09-18 14:02:15 +0800

    去掉多余依赖

--- TestNodePropose/make.sh
+++ TestNodePropose/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o TestNodePropose TestNodePropose.cc raft_test_util.cc -I ../include/ -I ../src/  -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
+g++ -std=c++11 -o TestNodePropose TestNodePropose.cc ../test/raft_test_util.cc -I ../include/ -I ../src/ -I ../test/ -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
--- TestTransferNonMember/make.sh
+++ TestTransferNonMember/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o TestTransferNonMember TestTransferNonMember.cc raft_test_util.cc -I ../include/ -I ../src/  -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
+g++ -std=c++11 -o TestTransferNonMember TestTransferNonMember.cc ../test/raft_test_util.cc -I ../include/ -I ../src/ -I ../test/ -L ../lib/ -L /usr/local/lib -lraft -lprotobuf -lpthread
